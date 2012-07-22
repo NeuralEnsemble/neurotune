01ed53cb3fc0bca160b398ac730d09db15e930c7
mike vella <mike@sunshine.(none)>
2012-07-22 05:51:57 +0100

    rearranging file hierarchy

--- optimal-neuron/controllers.py
+++ optimal-neuron/controllers.py
@@ -1,35 +1,109 @@
-import sqldbutils #should be all in controllers
+"""
+Still not implemented but will take controllers out of
+evaluators as it's the wrong place for them
+"""
+
 import os
 
-class __CandidateData(object):
-    """Container for information about a candidate (chromosome)"""
+class __Controller():
+    """
+    Controller must provide a run method
+    which returns exp_data type with a
+    exp_data.samples and exp_data.t attributes.
+    Now there is some very complicated technical stuff to
+    deal with here because my evaluators are not currently
+    decoupled from the implementation. I think what needs to
+    be done is that the controller object needs to accept a
+    list of params and chromosomes rather than just the one.
+    then whether it's parallel or not depends on the controller
+    rather than the evaluator, the evaluator can then just
+    logically become IClamp,VClamp etc... and all the
+    logic to do with whether it's implemented on a grid or not
+    goes down to the controller level.
+
+    To summarize my thoughts on this - the controller's job
+    is to control simulations. Unless it can accept lists of
+    parameters and chromosomes there is no way of parallelizing
+    the operation of a controller and parallelization will have
+    to fall to the Evaluator. The evaluator however should only
+    be concerned with assigining fitness to chromosomes (lower-
+    level stuff is, as stated, the controller's job) and as
+    such the controller needs to be able to accept lists of
+    chromosomes and parameters. This will allow much nicer
+    modularization, as long as the client can provide a controller
+    which will reutrn sample and time vectors for chromosome and parameter
+    lists any evaluator will be able to talk to it.
     
-    def __init__(self,chromosome):
-        self.chromosome=chromosome
-    
-    def set_dbpath(self,dbpath):
-        self.dbpath=dbpath
-        
-    def set_exp_id(self,exp_id):
+    as a slight modification perhaps it should be provided
+    with candidate objects rather than chromosomes
+    """
+
+    def run(self,candidates,parameters):
+        """
+        needs to accept a list of params and chromosomes
+        and return list of corresponding exp_data objects
+        """
+        raise NotImplementedError("Valid controller requires run method!")
+
+class CLIController(__Controller):
+    pass
+
+class NrnProject(__Controller):
+    """
+    Run an nrnproject simulation based on optimizer parameters
+    should we implement a singleton pattern?
+    I'm starting to question the sanity of this class, it
+    seems to me that it would make more sense to use a singleton
+    pattern here and do away with all those unecessary
+    calls to self
+    """
+
+    def __init__(self,nrnproject_path,db_path,exp_id=None):
+        self.sim_main_path=os.path.join(nrnproject_path,
+                                        'src/simrunner.py')
+        self.nrnproject_path=nrnproject_path
+        self.db_path=db_path
         self.exp_id=exp_id
     
-    def set_job_num(self,job_num):
-        self.job_num=job_num
-
-
-class __Evaluator(object):
-    """Base class for Evaluators"""
+    def __generate_cla(self):
+        sim_var_string = self.__generate_sim_var_string()
+        cla='python '+ self.sim_main_path + sim_var_string
+        return cla
     
-    def __init__(self,parameters,weights,targets,controller):
-
-        self.parameters=parameters
-        self.weights=weights
-        self.targets=targets
-        self.controller=controller
+    def __generate_sim_var_string(self):
+        sim_var_string=''
+        for i in enumerate(self.parameters):
+            sim_var_string+= ' "sim_var[\'' + i[1] +'\'] = '  + str(self.chromosome[i[0]]) + '\"'
+        
+        if self.exp_id !=None:
+            sim_var_string+= ' "sim_var[\'exp_id\'] ='+ str(self.exp_id) + '\"'
             
+        return sim_var_string
+    
+    def run(self,candidates,parameters):
+        import sqldbutils
+        exp_data_array=[]
+        for chromosome in candidates:
+            self.chromosome=chromosome
+            self.parameters=parameters
+            exp_id = sqldbutils.generate_exp_ids(self.db_path)
+            cla=self.__generate_cla()
+            os.chdir(self.nrnproject_path+'/src/') #there should be a smarter way
+            os.system(cla)
+            print self.db_path
+            print exp_id            
+            exp_data=sqldbutils.sim_data(self.db_path,exp_id)
+            exp_data_array.append(exp_data)
+        return exp_data_array
 
 class __CondorContext(object):
     """manager for dealing with a condor-based grid"""
+
+    def __init__(self,host,username,password,port):
+
+        self.messagehost=ssh_utils.host(host,username,
+                                        password,port)
+
         
     def __split_list(self,alist, wanted_parts=1):
         
@@ -113,7 +187,7 @@ class __CondorContext(object):
         #finally close the submit file
         submit_file.close()
     
-    def __build_condor_files(self,candidates,candidates_per_job=100):
+    def __build_condor_files(self,candidates,parameters,candidates_per_job=100):
         
         #prepare list of candidates to be farmed on grid:
         self.__prepare_candidates(candidates,candidates_per_job=100)
@@ -140,88 +214,34 @@ class __CondorContext(object):
             remotepath=os.path.join(remotedir,file)
             ssh_utils.put_file(host,localpath,remotepath)
 
-class IClampEvaluator(__Evaluator):
+
+class NrnProjectCondor(NrnProject):
     """
-    Locally-evaluates (not using cluster or grid computing) a model.
-    
-    The evaluate routine runs the model and returns its fitness value
-      
-    """
-    def __init__(self,analysis_start_time,controller,
-                 analysis_end_time,target_data_path,
-                 parameters,analysis_var,weights,
-                 targets=None,automatic=False):
-
-        super(IClampEvaluator, self).__init__(parameters,
-                                        weights,targets,
-                                        controller)
-      
-        self.analysis_start_time=analysis_start_time
-        self.analysis_end_time=analysis_end_time
-        self.target_data_path=target_data_path
-        self.analysis_var=analysis_var
-
-        if automatic==True:
-            from nrndev import nrnanalysis
-            import numpy
-            t,v_raw=nrnanalysis.load_csv_data(target_data_path)
-            v=numpy.array(v_raw)
-            v_smooth=list(nrnanalysis.smooth(v))
-            analysis=nrnanalysis.IClampAnalysis(
-                v_smooth,t,analysis_var,start_analysis=analysis_start_time,
-                end_analysis=analysis_end_time) 
-            analysis.analyse()
-            self.targets=analysis.analysis_results
-            print('Obtained targets are:')
-            print(self.targets)
-
-    def evaluate(self,candidates,args):
-        from nrndev import nrnanalysis
-        
-        exp_data_array = self.controller.run(candidates,self.parameters)
-        
-        for exp_data in exp_data_array:
-
-            analysis=nrnanalysis.IClampAnalysis(exp_data.samples,
-                                                exp_data.t,self.analysis_var,
-                                                self.analysis_start_time,
-                                                self.analysis_end_time,
-						target_data_path=self.target_data_path)
-
-            analysis.analyse()
-            exp_fitness=analysis.evaluate_fitness(self.targets,
-                                                  self.weights,cost_function=
-                                                  nrnanalysis.normalised_cost_function)
-            fitness.append(exp_fitness)
-
-        return fitness
-
-#this entire class should now be considered obsolete, the evaluator
-#is just an IClampEvaluator and everything here that is different
-#from that class needs to become its own controller
-class IClampCondorEvaluator(IClampEvaluator):
-    """
-    Evaluate simulations and return their fitness on a condor grid.
-    
-        Tested and known to work on CamGrid 
-        (http://www.escience.cam.ac.uk/projects/camgrid/)
-      
-    Dont inherit from condor evaluator - it's just a condor context
+    Run NrnProject-based simulations on a Condor-managed
+    federated system
     """
 
-    def __init__(self,local_analysis=False):
+    def __init__(self,host,username,password,port=80,
+                 local_analysis=False,candidates_per_job=100):
 
-        super(IClampCondorEvaluator,self).__init__()
+        super(NrnProjectCondor,self).__init__()
 
         #other things like the number of nodes to divide the work onto and
         #host connection parameters need to go into this constructor
+        #the more I think about it the less this seems like a good idea
+        #though
 
         if local_analysis:
-            self.evaluate=self.__local_evaluate
+            self.run=self.__local_run
         else:
-            self.evaluate=self.__remote_evaluate__
+            self.run=self.__remote_run__
 
-    def __condor_evaluate(self,candidates,args):
+        #make a context which provides grid utilities
+        self.context=__CondorContext(host,username,password,port)
+
+        self.cpj=candidates_per_job
+
+    def __condor_run(self,candidates,parameters):
         """
         Run simulations on grid and analyse data locally (???I'm quite confused here...there is a mistake somewhere as the name doesn't match the description - which method is which?)
       
@@ -232,29 +252,34 @@ class IClampCondorEvaluator(IClampEvaluator):
         """
         
         import time
-        from nrndev import nrnanalysis
         import ssh_utils
-        
-        self.CandidateData_list=[]
        
-        self.__build_condor_files(candidates) #Build submit and runx.sh files, exp_id now corresponds to position in chromosome and fitness arrays
+        #Build submit and runx.sh files, exp_id now corresponds 
+        #to position in chromosome and fitness arrays
+        self.context.__build_condor_files(candidates,parameters,
+                                          candidates_per_job=self.cpj) 
         
-        messagehost=ssh_utils.host(optimizer_params.host,optimizer_params.username,optimizer_params.password,optimizer_params.port)
-        
-        self.__delete_remote_files__(messagehost)#delete everything in thssh_utilse directory you're about to put files in
+        #This is a file handling block..
+        #delete everything in the ssh_utilse directory you're about to put files in
+        self.__delete_remote_files__()
         filelist=os.listdir(self.tmpdir)
-        self.__put_multiple_files(messagehost,filelist,localdir=self.tmpdir,remotedir=optimizer_params.remotedir)#copy local files over
+        #copy local files over, some stuff is missing here as it needs to be an attribute in the condor context
+        self.__put_multiple_files(filelist,localdir=self.tmpdir)
         filelist=os.listdir(self.portableswdir)
-        self.__put_multiple_files(messagehost,filelist,localdir=self.portableswdir,remotedir=optimizer_params.remotedir)#copy local files over
+        #copy portable software files over:
+        self.__put_multiple_files(filelist,localdir=self.portableswdir)
         
-        ssh_utils.issue_command(messagehost,'export PATH=/opt/Condor/release/bin:$PATH\ncondor_submit submitfile.submit')
+        #issue a command to the message host to issue commands to the grid:
+        ssh_utils.issue_command(context.messagehost,
+              'export PATH=/opt/Condor/release/bin:$PATH\ncondor_submit submitfile.submit')
         
+        #make a list of the database files we need:
         self.jobdbnames=[]
-        for job_num in range(self.num_jobs):        #make a list of the databases we need:
+        for job_num in range(self.num_jobs):
             jobdbname='outputdb'+str(job_num)+'.sqlite'
             self.jobdbnames.append(jobdbname)
            
-        #wait till you know file exists:
+        #wait till we know file exists:
         dbs_created=False
         pulled_dbs=[] # list of databases which have been extracted from remote server
         while (dbs_created==False):
@@ -262,27 +287,23 @@ class IClampCondorEvaluator(IClampEvaluator):
             time.sleep(20)            
             print 'checking if dbs created:'
             command='ls'
-            remote_filelist=ssh_utils.issue_command(messagehost, command)
-
+            remote_filelist=ssh_utils.issue_command(self.messagehost, command)
             for jobdbname in self.jobdbnames:
-
                 db_exists=jobdbname+'\n' in remote_filelist
-                
                 if (db_exists==False):
                     print jobdbname,' has not been generated'
                     dbs_created=False
-
                 elif db_exists==True and jobdbname not in pulled_dbs:
                     print jobdbname,' has been generated'
                     remotefile=optimizer_params.remotedir+jobdbname
                     localpath=os.path.join(self.datadir,str(self.generation)+jobdbname)
-                    ssh_utils.get_file(messagehost,remotefile,localpath)
+                    ssh_utils.get_file(self.messagehost,remotefile,localpath)
                     pulled_dbs.append(jobdbname) #so that it is not extracted more than once
                     #here pop-in the fitness evaluation
-
                 if len(pulled_dbs)==len(self.jobdbnames):
                     dbs_created=True
-            
+        
+        #this block can be simplified, it need simply return exp_data containers
         fitness=[]
         for CandidateData in self.CandidateData_list:
             job_num = CandidateData.job_num
@@ -308,15 +329,19 @@ class IClampCondorEvaluator(IClampEvaluator):
         self.generation+=1
         return fitness
 
+
+
+
+    ###ignore this for now###
     def __local_evaluate(self,candidates,args):
         import time
-        from nrndev import nrnanalysis
+        traceanalysis
      
         self.CandidateData_list=[]
         analysis_var=self.analysis_var
         
         #Build submitfile.submit and runx.sh files:
-        self.__build_condor_files(candidates) #exp_id now corresponds to position in chromosome/fitness array
+        self.__buil_condor_files(candidates) #exp_id now corresponds to position in chromosome/fitness array
         
         fitness=[]
         #submit the jobs to the grid
@@ -346,8 +371,8 @@ class IClampCondorEvaluator(IClampEvaluator):
             dbpath=self.datadir+dbname
             exp_id=CandidateData.exp_id
             exp_data=sqldbutils.sim_data(dbpath,exp_id)
-            analysis=nrnanalysis.IClampAnalysis(exp_data.samples,exp_data.t,analysis_var,5000,10000)
-            exp_fitness=analysis.evaluate_fitness(optimizer_params.targets,optimizer_params.weights,cost_function=nrnanalysis.normalised_cost_function)
+            analysis=traceanalysis.IClampAnalysis(exp_data.samples,exp_data.t,analysis_var,5000,10000)
+            exp_fitness=analysis.evaluate_fitness(self.targets,self.weights,cost_function=traceanalysis.normalised_cost_function)
             fitness.append(exp_fitness)
 
         for job_num in range(self.num_jobs):
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "optimal-neuron",
+    version = '0.1.1dev',
+    packages = ['optimal-neuron'],
+    author = "Michael Vella",
+    author_email = "mv333@cam.ac.uk",
+    description = "A Python library for working with NeuroML descriptions of neuronal models",
+    license = "BSD",
+    url='https://github.com/vellamike/Optimal-Neuron',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering']
+)
+
+
+

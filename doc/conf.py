1862b9a3e0dd36ac46ead6461ba87e18902f9b9f
Mike Vella <vellamike@gmail.com>
2012-12-07 17:30:52 +0000

    Update doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -304,6 +304,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['scipy','scipy.stats','inspyred','ec','numpy']
+MOCK_MODULES = ['scipy','scipy.stats','inspyred','ec','numpy','random','time','os']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()

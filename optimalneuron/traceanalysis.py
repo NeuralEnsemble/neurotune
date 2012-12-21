fa953b6241b49bdb31183f53e664fb40baea34a3
Mike Vella <vellamike@gmail.com>
2012-12-21 19:04:53 +0000

    doc bug

--- optimalneuron/traceanalysis.py
+++ optimalneuron/traceanalysis.py
@@ -30,7 +30,7 @@ def smooth(x,window_len=11,window='hanning'):
         
     .. example::
 
-    #Example usage:
+    >>> #Example usage:
     >>> t=linspace(-2,2,0.1)
     >>> x=sin(t)+randn(len(t))*0.1
     >>> y=smooth(x)

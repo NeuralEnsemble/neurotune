d7c2088f7f330c46e37a974db13fe54267a805d0
Mike Vella <vellamike@gmail.com>
2012-12-21 18:51:22 +0000

    documentation improvements

--- optimalneuron/traceanalysis.py
+++ optimalneuron/traceanalysis.py
@@ -4,7 +4,7 @@ Module for mathematical analysis of voltage traces from electrophysiology.
 
 AUTHORS:
 
-- Mike Vella (2011-04-08): initial version
+- Mike Vella vellamike@gmail.com
 
 """
 import scipy.stats
@@ -12,23 +12,21 @@ import numpy as np
 import math
 
 def smooth(x,window_len=11,window='hanning'):
-    """smooth the data using a window with requested size.
+    """Smooth the data using a window with requested size.
     
-    This is very useful for dealing with real data.
+    This is used for dealing with data.
+
     This method is based on the convolution of a scaled window with the signal.
     The signal is prepared by introducing reflected copies of the signal 
     (with the window size) in both ends so that transient parts are minimized
     in the begining and end part of the output signal.
     Params work very well with a tested whole-cell 20KHz signal.
     
-    input:
-        x: the input signal 
-        window_len: the dimension of the smoothing window; should be an odd integer
-        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
-            flat window will produce a moving average smoothing.
+    :param x: the input signal 
+    :param window_len: the dimension of the smoothing window; should be an odd integer
+    :param window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman', flat window will produce a moving average smoothing.
 
-    output:
-        the smoothed signal
+    :return the smoothed signal
         
     example:
 

def19df2fb44c0faa952af9cc2e966d4cf833341
Mike Vella <vellamike@gmail.com>
2013-01-14 16:26:56 +0000

    trying to fix math support

--- optimalneuron/traceanalysis.py
+++ optimalneuron/traceanalysis.py
@@ -434,7 +434,7 @@ def alpha_normalised_cost_function(value,target,base=10):
 
     .. math::
 
-      fitness=base**(-x)\\
+      fitness=base**(-x) \\
       x=((value-target)/(target+0.01))**2
       
     :param value: value measured

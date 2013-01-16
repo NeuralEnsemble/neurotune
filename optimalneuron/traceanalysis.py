d7000c7b8dd8184969655e5cc8fbb1b698382789
Mike Vella <vellamike@gmail.com>
2013-01-16 01:18:01 +0000

    back to mathjax..at wits end

--- optimalneuron/traceanalysis.py
+++ optimalneuron/traceanalysis.py
@@ -437,7 +437,7 @@ def alpha_normalised_cost_function(value,target,base=10):
 
       fitness=base^{-x}
 
-    where :math:`\sqrt{x^2-1}`
+    where :math:`\frac{x^2-1}{y}`
 
     .. math::
           x = {\frac{(value-target)}{(target + 0.01)^2}}

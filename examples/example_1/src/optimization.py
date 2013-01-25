a59dadf4abc5a7b684a39f8bbe8e32c473169fb6
Mike Vella <vellamike@gmail.com>
2013-01-25 02:04:16 +0000

    cleaning up example code

--- examples/example_1/src/optimization.py
+++ examples/example_1/src/optimization.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Simulation of Basket cell
-
+Automated optimization of simulation of Basket cell
 """
 
 __version__ = 0.1
@@ -12,19 +11,20 @@ import numpy as np
 from optimalneuron import optimizers
 from optimalneuron import evaluators
 from optimalneuron import controllers
-
+import os
 
 class Simulation(object):
 
     """
-    Simulation class inspired by Philipp Rautenberg
+    Simulation class - inspired by example of Philipp Rautenberg
 
         Objects of this class control a current clamp simulation. Example of use:
 
-            >>> cell = Cell()
+            >>> cell = Cell() #some kind of NEURON section
             >>> sim = Simulation(cell)
             >>> sim.go()
             >>> sim.show()
+
     """
 
     def __init__(self, recording_section, sim_time=1000, dt=0.05, v_init=-60):
@@ -61,20 +61,30 @@ class Simulation(object):
         self.rec_v.record(self.recording_section(0.5)._ref_v)
 
     def show(self):
+        """
+        Plot the result of the simulation once it's been intialized
+        """
+
         from matplotlib import pyplot as plt
+
         if self.go_already:
             x = np.array(self.rec_t)
             y = np.array(self.rec_v)
+
             plt.plot(x, y)
-            #plt.title("Hello World")
+            plt.title("Simulation voltage vs time")
             plt.xlabel("Time [ms]")
             plt.ylabel("Voltage [mV]")
-            #plt.axis(ymin=-120, ymax=-50)
+
         else:
             print("""First you have to `go()` the simulation.""")
         plt.show()
     
     def go(self, sim_time=None):
+        """
+        Start the simulation once it's been intialized
+        """
+
         self.set_recording()
         h.dt = self.dt
         
@@ -89,12 +99,23 @@ class Simulation(object):
 class BasketCellController():
 
     """
-    This is an example of "canonical controler"
+    This is a canonical example of a controller class
+
+    It provides a run() method, this run method must accept at least two parameters:
+        1. candidates (list of list of numbers)
+        2. The corresponding parameters. 
     """
 
     def run(self,candidates,parameters):
-        traces = []
+        """
+        Run simulation for each candidate
+        
+        This run method will loop through each candidate and run the simulation
+        corresponding to it's parameter values. It will populate an array called
+        traces with the resulting voltage traces for the simulation and return it.
+        """
 
+        traces = []
         for candidate in candidates:
             sim_var = dict(zip(parameters,candidate))
             t,v = self.run_individual(sim_var)
@@ -103,11 +124,25 @@ class BasketCellController():
         return traces
 
     def set_section_mechanism(self, sec, mech, mech_attribute, mech_value):
+        """
+        Set the value of an attribute of a NEURON section
+        """
         for seg in sec:
             setattr(getattr(seg, mech), mech_attribute, mech_value)
     
     def run_individual(self,sim_var):
-  
+        """
+        Run an individual simulation.
+
+        The candidate data has been flattened into the sim_var dict. The
+        sim_var dict contains parameter:value key value pairs, which are
+        applied to the model before it is simulated.
+
+        The simulation itself is carried out via the instantiation of a
+        Simulation object (see Simulation class above).
+
+        """
+
         #make compartments and connect them
         soma=h.Section()
         axon=h.Section()
@@ -127,8 +162,6 @@ class BasketCellController():
     
         #soma.insert('canrgc')
         #soma.insert('cad2')
-    
-        #nrntools.set_section_mechanism(sec,'kv','gbar',gkv_dend)
     
         self.set_section_mechanism(axon,'na','gbar',sim_var['axon_gbar_na'])
         self.set_section_mechanism(axon,'kv','gbar',sim_var['axon_gbar_kv'])
@@ -155,62 +188,92 @@ class BasketCellController():
     
 
 
-sim_var={}
 
-sim_var['axon_gbar_na']    = 1000.0
-sim_var['axon_gbar_kv']    = 2310.0
-sim_var['axon_gbar_kv3']   = 0.0
-sim_var['soma_gbar_na']    = 30.0
-sim_var['soma_gbar_kv']    = 220.0
-sim_var['soma_gbar_kv3']   = 330.0
-
-my_controller= BasketCellController()
-
-"""
-Script to optimize Basket cell current injection response
-"""
-
-
-#first off we need to make an evaluator,
-
-
-parameters = ['axon_gbar_na','axon_gbar_kv','axon_gbar_kv3','soma_gbar_na','soma_gbar_kv','soma_gbar_kv3']
-
-#manual_vals=[50,50,2000,70,70,5,0.1,28.0,49.0,-73.0,23.0] # EXAMPLE - how to set a seed
-min_constraints = [0,0,0,0,0,0]
-max_constraints = [10000,30,1,300,20,2]
-
-analysis_var={'peak_delta':0,'baseline':0,'dvdt_threshold':2}
-
-weights={'average_minimum': 1.0, 'spike_frequency_adaptation': 1.0, 'trough_phase_adaptation': 1.0, 'mean_spike_frequency': 1.0, 'average_maximum': 1.0, 'trough_decay_exponent': 1.0, 'interspike_time_covar': 1.0, 'min_peak_no': 1.0, 'spike_broadening': 1.0, 'spike_width_adaptation': 1.0, 'max_peak_no': 1.0, 'first_spike_time': 1.0, 'peak_decay_exponent': 1.0,'pptd_error':1.0}
+def main():    
+    """
+    The optimization runs in this main method
+    """
+    
+    #make a controller
+    my_controller= BasketCellController()
+    
+    #parameters to be modified in each simulation
+    parameters = ['axon_gbar_na',
+                  'axon_gbar_kv',
+                  'axon_gbar_kv3',
+                  'soma_gbar_na',
+                  'soma_gbar_kv',
+                  'soma_gbar_kv3']
+    
+    #above parameters will not be modified outside these bounds:
+    min_constraints = [0,0,0,0,0,0]
+    max_constraints = [10000,30,1,300,20,2]
 
 
-targets={'average_minimum': -38.839498793604541, 'spike_frequency_adaptation': 0.019619800882894008, 'trough_phase_adaptation': 0.005225712358530369, 'mean_spike_frequency': 47.353760445682454, 'average_maximum': 29.320249266525668, 'trough_decay_exponent': 0.11282542321257279, 'interspike_time_covar': 0.042610190921388166, 'min_peak_no': 34, 'spike_broadening': 0.81838856772318913, 'spike_width_adaptation': 0.0095057081186080035, 'max_peak_no': 35, 'first_spike_time': 164.0, 'peak_decay_exponent': -0.04596529555434687,'pptd_error':0}
+    # EXAMPLE - how to set a seed
+    #manual_vals=[50,50,2000,70,70,5,0.1,28.0,49.0,-73.0,23.0] 
 
-#using automatic target evaluation:
-#what we should do next is separate out the controller and pass it as an object to the evaluator-
-#we really need to think about this deeply, separating the nrnproject logic from the neuronoptimizer
-#may be quite hard
+    #analysis variables, these default values will do:
+    analysis_var={'peak_delta':0,
+                  'baseline':0,
+                  'dvdt_threshold':2}
+    
 
-#remember, under my new design ideas evaluator is decoupled from the implementation as this is a job for controller
 
-import os
+#    targets={'average_minimum': -38.839498793604541,
+#             'spike_frequency_adaptation': 0.019619800882894008,
+#             'trough_phase_adaptation': 0.005225712358530369,
+#             'mean_spike_frequency': 47.353760445682454,
+#             'average_maximum': 29.320249266525668,
+#             'trough_decay_exponent': 0.11282542321257279,
+#             'interspike_time_covar': 0.042610190921388166,
+#             'min_peak_no': 34,
+#             'spike_broadening': 0.81838856772318913,
+#             'spike_width_adaptation': 0.0095057081186080035,
+#             'max_peak_no': 35,
+#             'first_spike_time': 164.0,
+#             'peak_decay_exponent': -0.04596529555434687,
+#             'pptd_error':0}
 
-my_evaluator=evaluators.IClampEvaluator(controller=my_controller,
-                                        analysis_start_time=1,
-                                        analysis_end_time=500,
-                                        target_data_path='../experimental_data/100pA_1.csv',
-                                        parameters=parameters,
-                                        analysis_var=analysis_var,
-                                        weights=weights,
-                                        targets=targets,
-                                        automatic=True)
 
-my_optimizer=optimizers.CustomOptimizerA(max_constraints,min_constraints,my_evaluator,
-                                  population_size=3,
-                                  max_evaluations=100,
-                                  num_selected=3,
-                                  num_offspring=3,
-                                  num_elites=1,
-                                  seeds=None)
-my_optimizer.optimize()
+    weights={'average_minimum': 1.0,
+             'spike_frequency_adaptation': 1.0,
+             'trough_phase_adaptation': 1.0,
+             'mean_spike_frequency': 1.0,
+             'average_maximum': 1.0,
+             'trough_decay_exponent': 1.0,
+             'interspike_time_covar': 1.0,
+             'min_peak_no': 1.0,
+             'spike_broadening': 1.0,
+             'spike_width_adaptation': 1.0,
+             'max_peak_no': 1.0,
+             'first_spike_time': 1.0,
+             'peak_decay_exponent': 1.0,
+             'pptd_error':1.0}
+    
+    
+    
+    #make an evaluator, using automatic target evaluation:
+    my_evaluator=evaluators.IClampEvaluator(controller=my_controller,
+                                            analysis_start_time=1,
+                                            analysis_end_time=500,
+                                            target_data_path='../experimental_data/100pA_1.csv',
+                                            parameters=parameters,
+                                            analysis_var=analysis_var,
+                                            weights=weights,
+                                            targets=targets,
+                                            automatic=True)
+
+    #make an optimizer
+    my_optimizer=optimizers.CustomOptimizerA(max_constraints,min_constraints,my_evaluator,
+                                      population_size=3,
+                                      max_evaluations=100,
+                                      num_selected=3,
+                                      num_offspring=3,
+                                      num_elites=1,
+                                      seeds=None)
+
+    #run the optimizer
+    my_optimizer.optimize()
+
+main()

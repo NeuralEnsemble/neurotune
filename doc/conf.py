6b4ceacd422e5b2e99c94fcb45402ddccced7924
Mike Vella <vellamike@gmail.com>
2014-05-19 23:23:08 +0100

    fixed mocking time module which was used by sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -296,6 +296,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['scipy','scipy.stats','inspyred','inspyred.ec','ec','numpy','time','neuron']
+MOCK_MODULES = ['scipy','scipy.stats','inspyred','inspyred.ec','ec','numpy','neuron']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()

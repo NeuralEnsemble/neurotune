ca3dcd4731109447df001cf2bc4b827c9b5ad486
Padraig Gleeson <p.gleeson@gmail.com>
2015-02-04 07:57:54 +0000

    Minor changes to analysis examples

--- examples/example_1/data_analysis.py
+++ examples/example_1/data_analysis.py
@@ -1,7 +1,8 @@
 from pyelectro import analysis as pye_analysis
 from matplotlib import pyplot
 
-t,v=pye_analysis.load_csv_data('100pA_1.csv')
+file_name = '100pA_1.csv'
+t,v=pye_analysis.load_csv_data(file_name)
 
 analysis_var={'peak_delta':0.1,'baseline':0,'dvdt_threshold':2,'peak_threshold':0}
 
@@ -16,4 +17,5 @@ res = analysis.analyse()
 print res
 
 pyplot.plot(t,v)
+pyplot.suptitle('Data read in from: %s'%file_name)
 pyplot.show()
